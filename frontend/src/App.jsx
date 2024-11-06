d793a800aabdf757fe7de50d70c401266adad90a
AllenRyan <allenisop786@gmail.com>
2024-11-06 10:46:33 +0500

    3rd commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import Hero from './components/Hero'
+
 
 function App() {
-  
+  <Hero />
 }
 
 export default App
--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React from 'react';
+import logo from './public/images/logo.png';
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <header className='header'>
+       <nav className="className">
+        <div className="logo">
+            <img src={logo} alt="Logo" />
+        </div>
+       </nav>
+    </header>
   )
 }
 
